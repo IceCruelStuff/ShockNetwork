1a254e749056b81fd2629e4de8a36ec0947315c6
Drew <buchwalds@tutanota.com>
2020-05-12 01:04:25 -0400

    Add files via upload

---
+++ ShockHub/src/hub/Loader.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace hub;
+
+use hub\tasks\SideBarTask;
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase{
+
+	public function onEnable(){
+		$this->getScheduler()->scheduleRepeatingTask(new SideBarTask($this), 20);
+		new EventListener($this);
+	}
+}
